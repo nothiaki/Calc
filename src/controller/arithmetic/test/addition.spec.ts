a075a4bffdaf80a70fb28195d06692b04e272e3e
nothiaki <celsohenrique367@gmail.com>
2024-04-23 18:23:32 -0300

    feat: jest

---
+++ src/controller/arithmetic/test/addition.spec.ts
@@ -0,0 +1,10 @@
+//import axios from 'axios';
+
+//or additionController
+describe('GET /arithmetic/addition/?nums=x,y', () => {
+  it('should return sum of nums', async () => {
+    //const res = await request.get('http://localhost:3000/arithmetic/addition/?nums=0,1');
+
+    //expect(res.status).toEqual(200);
+  });
+});
